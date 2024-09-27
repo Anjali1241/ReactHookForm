d01386367de4535c70f7a0b37d62aafb438107a0
Anjali Sharma <sharmaanjali1241@gmail.com>
2024-09-28 01:57:30 +0530

    Add validation schema

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,62 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"; 
+import * as yup from "yup";
 
 function Form() {
-  const { register, handleSubmit, reset, watch, setValue, getValues } =
-    useForm();
-  const onSubmit = (data) => {
+  const schema = yup.object({
+    firstName: yup.string().required("First Name is a required field"),
+    lastName: yup.string().required("Last Name is a required field"),
+    password: yup
+      .string()
+      .required("Password is a required field")
+      .min(8, "Password length must be at least 8 characters"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      password: "",
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     console.log("Form Data Submitted:", data);
     reset();
   };
+
   return (
     <>
       <div>Form</div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="firstName">FirstName</label>
-        <input type="text" name="firstName" {...register("firstName")} />
-        <label htmlFor="lastName">LastName</label>
-        <input type="text" name="lastName" {...register("lastName")} />
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          {...register("firstName")} 
+        />
+        <br></br>
+        {errors.firstName && <div>{errors.firstName.message}</div>}
+
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" id="lastName" {...register("lastName")} />
+        <br></br>
+        {errors.lastName && <div>{errors.lastName.message}</div>}
+
         <label htmlFor="password">Password</label>
-        <input type="password"  {...register("password")}/>
-        <button type="submit"> Submit</button>
+        <input type="password" id="password" {...register("password")} />
+        <br></br>
+        {errors.password && <div>{errors.password.message}</div>}
+
+        <button type="submit"> {isSubmitting ? "Loading" : "Submit"}</button>
       </form>
     </>
   );
