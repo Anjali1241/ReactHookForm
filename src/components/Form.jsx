c112f011f5cf364217cec07b4879f811c391e28a
Anjali Sharma <sharmaanjali1241@gmail.com>
2024-09-28 01:25:12 +0530

    update code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 function Form() {
-    const {register
-        ,handleSubmit,reset,watch
-        setValue,getValues
-
-    }=useForm()
-    const onSubmit=(data)=>{
-        console.log("Form Data Submitted:", data);
-        reset(); 
-    }
+  const { register, handleSubmit, reset, watch, setValue, getValues } =
+    useForm();
+  const onSubmit = (data) => {
+    console.log("Form Data Submitted:", data);
+    reset();
+  };
   return (
     <>
       <div>Form</div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" name="firstName" />
-        <input type="text" name="lastName" />
-        <input type="password" />
+        <label htmlFor="firstName">FirstName</label>
+        <input type="text" name="firstName" {...register("firstName")} />
+        <label htmlFor="lastName">LastName</label>
+        <input type="text" name="lastName" {...register("lastName")} />
+        <label htmlFor="password">Password</label>
+        <input type="password"  {...register("password")}/>
         <button type="submit"> Submit</button>
       </form>
     </>
